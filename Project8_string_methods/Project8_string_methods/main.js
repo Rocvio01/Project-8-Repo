85751ad928b463a6451ce86b6674f01c7acfca70
Rocvio01 <162543781+Rocvio01@users.noreply.github.com>
2024-05-09 22:11:11 -0700

    Add files via upload

---
+++ Project8_string_methods/Project8_string_methods/main.js
@@ -0,0 +1,18 @@
+let greeting = "Hello";
+let name = "Tavio"
+let str
+str = greeting + " " + name + ".";
+str = greeting.concat("", name, ".");
+function slice_Method() {
+    var Sentence = "No playing soccer make the week so much longer.";
+    var Section = Sentence_slide(7, 8);
+    document.getElementById("Slice") .innerHTML = Section
+}
+function string_Method() {
+    var X =182;
+    document.getElementById("Numbers_to_string") .innerHTML = X.toString();
+}
+
+function precision_Method() {
+    var X = 12938.3012987376112
+}
